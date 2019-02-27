6106bd73836665e0d08fb2029dcbefddb60f164a
Yana <m.yana93@mail.ru>
2019-02-27 09:53:54 +0300

    Исправление ошибок по сеткам

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var show_btn = document.querySelector("button[name=show-form]");
+var hotel_form = document.querySelector(".hotel-form");
+var found_btn = document.querySelector(".btn-found");
+// console.log(hotel_form)
+
+show_btn.onclick = function() {
+  console.log("CLICK!");
+  hotel_form.classList.add(".show");
+};
