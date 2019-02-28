a9c52e3cd7327fb2722e96edbfc0d15707190ab3
Yana <m.yana93@mail.ru>
2019-02-28 22:42:38 +0300

    Декоративные элементы: исправление ошибок

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 var show_btn = document.querySelector("button[name=show-form]");
 var hotel_form = document.querySelector(".hotel-form");
 var found_btn = document.querySelector(".btn-found");
-// console.log(hotel_form)
+var plus_btn = document.querySelector("button[name=button-plus]");
+var minus_btn = document.querySelector("button[name=button-minus]");
+var adults = document.querySelector("input[name=count-adults]");
+var children = document.querySelector("input[name=count-children]");
+
+// localStorage {key: "adults", length: adults.value }
 
 show_btn.onclick = function() {
   console.log("CLICK!");
-  hotel_form.classList.add("show");
+  hotel_form.classList.toggle("visually-hidden");
+  hotel_form.classList.toggle("show");
 };
+
+
+plus_btn.onclick = function() {
+  adults.value++;
+  // console.log(previousElementSibling[0]);
+  adults.value++;
+}
+
+minus_btn.onclick = function() {
+  adults.value--;
+  if(adults.value < 0) {
+    adults.value = 0;
+  }
+}
